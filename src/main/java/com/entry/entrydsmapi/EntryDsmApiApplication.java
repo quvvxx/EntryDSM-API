719ef964bf102ca62c55b131737bc00423cf9338
quvvxx <quvvxx2@gmail.com>
2026-07-11 21:57:17 +0900

    chore: 프로젝트 초기 세팅

---
+++ src/main/java/com/entry/entrydsmapi/EntryDsmApiApplication.java
@@ -0,0 +1,13 @@
+package com.entry.entrydsmapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntryDsmApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EntryDsmApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/entry/entrydsmapi/EntryDsmApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.entry.entrydsmapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntryDsmApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
