e78dbfd5d7d2198759f38a9921cd25d073375c42
quvvxx <quvvxx2@gmail.com>
2026-07-15 00:12:16 +0900

    feat: 전역 예외 처리 및 에러 응답 구현

---
+++ src/main/java/com/entry/entrydsmapi/global/exception/ErrorResponse.java
@@ -0,0 +1,12 @@
+package com.entry.entrydsmapi.global.exception;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class ErrorResponse {
+
+    private String code;
+    private String message;
+}
---
+++ src/main/java/com/entry/entrydsmapi/global/exception/GlobalExceptionHandler.java
@@ -0,0 +1,50 @@
+package com.entry.entrydsmapi.global.exception;
+
+import com.entry.entrydsmapi.domain.user.exception.InvalidCredentialsException;
+import com.entry.entrydsmapi.domain.user.exception.PhoneAlreadyExistsException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(InvalidCredentialsException.class)
+    public ResponseEntity<ErrorResponse> HandleInvalidCredentials(InvalidCredentialsException e){
+
+        ErrorResponse response = ErrorResponse.builder()
+                .code("INVALID_CREDENTIALS")
+                .message(e.getMessage())
+                .build();
+
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                .body(response);
+    }
+
+    @ExceptionHandler(PhoneAlreadyExistsException.class)
+    public ResponseEntity<ErrorResponse> HandlePhoneAlreadyExists(PhoneAlreadyExistsException e){
+
+        ErrorResponse response = ErrorResponse.builder()
+                .code("PHONE_ALREADY_EXISTS")
+                .message(e.getMessage())
+                .build();
+
+        return ResponseEntity.status(HttpStatus.CONFLICT)
+                .body(response);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> HandlerMethodArgumentNotValid(MethodArgumentNotValidException e){
+        ErrorResponse response = ErrorResponse.builder()
+                .code("INVALID_INPUT")
+                .message(e.getBindingResult()
+                        .getFieldError()
+                        .getDefaultMessage())
+                .build();
+        
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(response);
+    }
+}
