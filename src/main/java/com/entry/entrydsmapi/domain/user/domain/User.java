9c1c5b1b31023a10a0c3f965c0bfe3da4ee830c3
quvvxx <quvvxx2@gmail.com>
2026-07-15 00:08:35 +0900

    refactor: User 엔티티 생성 방식을 빌더 패턴으로 변경

--- src/main/java/com/entry/entrydsmapi/domain/user/domain/User.java
+++ src/main/java/com/entry/entrydsmapi/domain/user/domain/User.java
@@ -1,10 +1,13 @@
 package com.entry.entrydsmapi.domain.user.domain;
 
 import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @NoArgsConstructor
+@Getter
 @Table(name = "users")
 public class User {
 
@@ -23,4 +26,13 @@ public class User {
     @Column(nullable = false, length = 50)
     @Enumerated(EnumType.STRING)
     private Role role;
+
+
+    @Builder
+    private User(String name, String phone, String password, Role role){
+        this.name = name;
+        this.phone = phone;
+        this.password = password;
+        this.role = role;
+    }
 }
