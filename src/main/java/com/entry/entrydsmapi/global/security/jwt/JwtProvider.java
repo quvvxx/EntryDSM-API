cb13d030d66147ca68ad16389287c6c649fa4a87
quvvxx <quvvxx2@gmail.com>
2026-07-15 00:17:53 +0900

    feat: JWT 액세스 토큰 발급 구현

---
+++ src/main/java/com/entry/entrydsmapi/global/security/jwt/JwtProvider.java
@@ -0,0 +1,40 @@
+package com.entry.entrydsmapi.global.security.jwt;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+@Component
+public class JwtProvider {
+
+    @Value("${jwt.secret}")
+    private String secretKeyString;
+
+    @Value("${jwt.access-token-expiration}")
+    private Long validityInMilliseconds;
+
+    private SecretKey secretKey;
+
+    @PostConstruct
+    private void init() {
+        this.secretKey = Keys.hmacShaKeyFor(secretKeyString.getBytes(StandardCharsets.UTF_8));
+    }
+
+    public String generateAccessToken(Long userId){
+        Date now = new Date();
+        Date expiration = new Date(now.getTime() + validityInMilliseconds);
+
+        return Jwts.builder()
+                .subject(userId.toString())
+                .issuedAt(now)
+                .expiration(expiration)
+                .signWith(secretKey)
+                .compact();
+    }
+}
