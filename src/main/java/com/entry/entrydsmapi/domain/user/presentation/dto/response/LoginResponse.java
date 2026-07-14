3a475c4a57e473abf5aaf80d46ee57237cbaae70
quvvxx <quvvxx2@gmail.com>
2026-07-15 00:12:48 +0900

    feat: 로그인 응답 DTO 추가

---
+++ src/main/java/com/entry/entrydsmapi/domain/user/presentation/dto/response/LoginResponse.java
@@ -0,0 +1,11 @@
+package com.entry.entrydsmapi.domain.user.presentation.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class LoginResponse {
+
+    private String accessToken;
+}
