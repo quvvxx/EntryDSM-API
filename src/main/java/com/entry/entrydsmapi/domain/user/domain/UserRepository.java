5edf1fe3859ee994936c63a163bd6daca5d27b8b
quvvxx <quvvxx2@gmail.com>
2026-07-15 00:09:46 +0900

    feat: UserRepository 전화번호 중복 확인 및 조회 메서드 추가

--- src/main/java/com/entry/entrydsmapi/domain/user/domain/UserRepository.java
+++ src/main/java/com/entry/entrydsmapi/domain/user/domain/UserRepository.java
@@ -2,5 +2,10 @@ package com.entry.entrydsmapi.domain.user.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    boolean existsByPhone(String phone);
+    Optional<User> findByPhone(String phone);
 }
