7895bd568b3b132210fad0951fe2220a634c0805
quvvxx <quvvxx2@gmail.com>
2026-07-15 00:58:26 +0900

    feat: 인증/인가를 위한 Spring Security 기본 설정 추가

--- src/main/java/com/entry/entrydsmapi/global/config/SecurityConfig.java
+++ src/main/java/com/entry/entrydsmapi/global/config/SecurityConfig.java
@@ -2,14 +2,31 @@ package com.entry.entrydsmapi.global.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        return http.csrf(csrf -> csrf.disable())
+                .formLogin(form -> form.disable())
+                .httpBasic(httpBasic -> httpBasic.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/auth/signup", "/auth/login")
+                        .permitAll()
+                        .anyRequest().authenticated()
+                ).build();
     }
 }
