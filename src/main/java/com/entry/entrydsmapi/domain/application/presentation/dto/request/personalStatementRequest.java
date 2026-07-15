f14c3da21f931f0450840804f9604d3b34edf4b2
quvvxx <quvvxx2@gmail.com>
2026-07-16 01:19:13 +0900

    feat: 자기소개서 작성·수정 요청 DTO 추가

---
+++ src/main/java/com/entry/entrydsmapi/domain/application/presentation/dto/request/personalStatementRequest.java
@@ -0,0 +1,16 @@
+package com.entry.entrydsmapi.domain.application.presentation.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class personalStatementRequest {
+
+    @NotBlank(message = "자기소개서를 작성해주세요.")
+    @Size(max = 1600, message = "자기소개서는 1600자 이하로 작성해야 합니다.")
+    private String personalStatement;
+
+}
