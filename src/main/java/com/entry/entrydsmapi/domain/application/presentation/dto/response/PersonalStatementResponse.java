70eccfa936272c0dff8d0528f8a18e5bdb92fee0
quvvxx <quvvxx2@gmail.com>
2026-07-16 14:13:44 +0900

    feat: 자기소개서 조회 응답 DTO 추가

---
+++ src/main/java/com/entry/entrydsmapi/domain/application/presentation/dto/response/PersonalStatementResponse.java
@@ -0,0 +1,12 @@
+package com.entry.entrydsmapi.domain.application.presentation.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class PersonalStatementResponse {
+
+    private String personalStatement;
+
+}
