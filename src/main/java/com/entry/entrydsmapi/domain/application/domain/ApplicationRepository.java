4908cfe2f6709f9a7ce174d9996dd464ab14fe90
quvvxx <quvvxx2@gmail.com>
2026-07-17 16:19:26 +0900

    feat: 사용자와 지원 연도로 지원서 조회 기능 추가

--- src/main/java/com/entry/entrydsmapi/domain/application/domain/ApplicationRepository.java
+++ src/main/java/com/entry/entrydsmapi/domain/application/domain/ApplicationRepository.java
@@ -1,6 +1,9 @@
 package com.entry.entrydsmapi.domain.application.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
 public interface ApplicationRepository extends JpaRepository<Application, Long> {
+
+    Optional<Application> findByUserIdAndApplicationYear(Long userId, Integer applicationYear);
 }
