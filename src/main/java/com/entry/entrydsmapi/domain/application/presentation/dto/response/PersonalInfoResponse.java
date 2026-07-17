5443f06bd1c2b39c16fa0aba420c235b268acd65
quvvxx <quvvxx2@gmail.com>
2026-07-17 20:41:29 +0900

    feat: 생년월일 데이터 직렬화를 위한 @JsonFormat 추가

--- src/main/java/com/entry/entrydsmapi/domain/application/presentation/dto/response/PersonalInfoResponse.java
+++ src/main/java/com/entry/entrydsmapi/domain/application/presentation/dto/response/PersonalInfoResponse.java
@@ -2,6 +2,7 @@ package com.entry.entrydsmapi.domain.application.presentation.dto.response;
 
 import com.entry.entrydsmapi.domain.application.domain.Gender;
 import com.entry.entrydsmapi.domain.application.domain.Region;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Builder;
 import lombok.Getter;
 import java.time.LocalDate;
@@ -10,6 +11,7 @@ import java.time.LocalDate;
 @Builder
 public class PersonalInfoResponse {
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
     private LocalDate birthDate;
     private String applicantName;
     private Gender gender;
