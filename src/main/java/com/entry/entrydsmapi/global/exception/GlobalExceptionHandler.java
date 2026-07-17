34965993bae7198945a35d080d65312086b353c0
quvvxx <quvvxx2@gmail.com>
2026-07-17 14:06:20 +0900

    feat: GlobalExceptionHandler에 UserNotFoundException 404 처리 추가

--- src/main/java/com/entry/entrydsmapi/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/entry/entrydsmapi/global/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.entry.entrydsmapi.global.exception;
 
 import com.entry.entrydsmapi.domain.user.exception.InvalidCredentialsException;
 import com.entry.entrydsmapi.domain.user.exception.PhoneAlreadyExistsException;
+import com.entry.entrydsmapi.domain.user.exception.UserNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -45,6 +46,18 @@ public class GlobalExceptionHandler {
                 .build();
         
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(response);
+
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<ErrorResponse> HandlerUserNotFound(UserNotFoundException e){
+        ErrorResponse response = ErrorResponse.builder()
+                .code("USER_NOT_FOUND")
+                .message(e.getMessage())
+                .build();
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
                 .body(response);
     }
 }
