ebc8b3adf73b9d0301724bef9e55f4994802347e
quvvxx <quvvxx2@gmail.com>
2026-07-17 16:16:39 +0900

    feat: 지원서 최종 제출 및 지원서 삭제 기능 구현

---
+++ src/main/java/com/entry/entrydsmapi/domain/application/service/ApplicationService.java
@@ -0,0 +1,40 @@
+package com.entry.entrydsmapi.domain.application.service;
+
+import com.entry.entrydsmapi.domain.application.domain.Application;
+import com.entry.entrydsmapi.domain.application.domain.ApplicationRepository;
+import com.entry.entrydsmapi.domain.application.domain.Status;
+import com.entry.entrydsmapi.domain.application.exception.ApplicationNotFoundException;
+import com.entry.entrydsmapi.domain.user.domain.User;
+import com.entry.entrydsmapi.domain.user.facade.UserFacade;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class ApplicationService {
+
+    private final ApplicationRepository applicationRepository;
+    private final UserFacade userFacade;
+
+    public void deleteApplication(int year){
+
+        User user = userFacade.getCurrentUser();
+
+        Application application = applicationRepository.findByUserIdAndApplicationYear
+                (user.getId(), year).orElseThrow(ApplicationNotFoundException::new);
+
+        applicationRepository.delete(application);
+    }
+
+    public void statusApplication(int year){
+
+        User user = userFacade.getCurrentUser();
+
+        Application application = applicationRepository.findByUserIdAndApplicationYear
+                (user.getId(), year).orElseThrow(ApplicationNotFoundException::new);
+
+        application.submit();
+    }
+}
